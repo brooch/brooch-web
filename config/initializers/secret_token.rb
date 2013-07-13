4e63c681e9a2e5b8f62404749c096d2ce2cc76aa
Kentaro Kuribayashi <kentarok@gmail.com>
2013-07-13 13:33:40 +0900

    rails new -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Brooch::Application.config.secret_key_base = '3b77263a40b46c28b935272b86670ad7b0535a688d901872d727202fcf836365248ab3139740cc86ba7e39709c2fc9c0f18243ae3080c3ae6640dd15d58be614'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Brooch::Application.config.session_store :cookie_store, key: '_brooch_session'
